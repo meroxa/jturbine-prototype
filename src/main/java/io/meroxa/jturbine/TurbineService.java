3ca5e35615d5e644cfe69fab91420a9789032b2c
Haris Osmanagic <haris@meroxa.io>
2023-06-02 15:22:19 +0200

    skeleton, TurbineService.Init

---
+++ src/main/java/io/meroxa/jturbine/TurbineService.java
@@ -0,0 +1,16 @@
+package io.meroxa.jturbine;
+
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import io.meroxa.turbine.core.InitRequest;
+import io.quarkus.grpc.GrpcService;
+
+import static io.meroxa.turbine.core.TurbineServiceGrpc.*;
+
+@GrpcService
+public class TurbineService extends TurbineServiceImplBase {
+    @Override
+    public void init(InitRequest request, StreamObserver<Empty> responseObserver) {
+        super.init(request, responseObserver);
+    }
+}
