416b6811729c1a1ce1b758b757cb91984d5728e4
Haris Osmanagic <haris@meroxa.io>
2023-06-02 16:04:20 +0200

    add main

--- src/main/java/io/meroxa/turbine/Main.java
+++ src/main/java/io/meroxa/turbine/Main.java
@@ -1,7 +1,14 @@
 package io.meroxa.turbine;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+
+public class Main implements QuarkusApplication {
+    @Override
+    public int run(String... args) {
+        System.out.println("inside io.meroxa.turbine.Main");
+        Quarkus.waitForExit();
+
+        return 0;
     }
 }
