2cb9984496834d35e013c0ea590c069dc0f175d3
Raúl Barroso <ra.barroso@gmail.com>
2023-10-18 16:33:54 +0200

    Update Main.java

--- src/main/java/com/meroxa/turbine/Main.java
+++ src/main/java/com/meroxa/turbine/Main.java
@@ -6,7 +6,7 @@ import io.quarkus.runtime.QuarkusApplication;
 public class Main implements QuarkusApplication {
     @Override
     public int run(String... args) {
-        System.out.println("inside io.meroxa.turbine.Main");
+        System.out.println("inside com.meroxa.turbine.Main");
         Quarkus.waitForExit();
 
         return 0;
